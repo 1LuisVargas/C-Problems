78733bff86e47e27f066f85f62a98ee05d567983
Luis Vargas <45537571+1LuisVargas@users.noreply.github.com>
2023-04-21 21:52:13 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/*Created by Luis Vargas, 31st of January 2023
+This is part of Harvard's CS50 course problems*/
+
+int main(void)
+{
+    string name = get_string("Tell me your name\n");
+    printf("hello %s\n", name); //Greets the user
+}
